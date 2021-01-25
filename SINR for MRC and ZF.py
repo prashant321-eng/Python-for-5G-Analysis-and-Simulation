b5efe7091c4a6bac1c1ff800b84b18aa36ce1039
Prashant kumar <71422162+prashant321-eng@users.noreply.github.com>
2021-01-25 19:27:04 +0530

    Add files via upload

---
+++ SINR for MRC and ZF.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 20:56:14 2020
+
+@author: pc
+"""
+
+
+import numpy as np
+import numpy.random as nr
+import numpy.linalg as nl
+import matplotlib.pyplot as plt
+import m5G
+
+ITER = 2000;
+K = 10; # number of users
+Mv = np.arange(20,520,30); # number of BS antennas
+Eu_dB = 10;  Eu = 10**(Eu_dB/10);
+rate_MRC = np.zeros(len(Mv)) ;
+bound_MRC = np.zeros(len(Mv));
+rate_ZF = np.zeros(len(Mv));
+
+beta = m5G.Dmatrix(K);
+sqrtD = np.diag(np.sqrt(beta));
+c=0;
+for it in range(ITER):
+    c=c+1;
+    print(c);
+    for mx in range(len(Mv)):
+        M=Mv[mx];
+        #pu=Eu;
+        pu=Eu/M;
+        H=(nr.normal(0.0,1.0,(M,K))+1j*nr.normal(0.0,1.0,(M,K)))/np.sqrt(2);
+        G=np.matmul(H,sqrtD);
+        g0=G[:,0];
+        MRCbf=g0/nl.norm(g0);
+        nr_MRC=pu*nl.norm(g0)**2;
+        nr_bound_MRC=pu*M*beta[0];
+        dr_bound_MRC=1;
+        mu_int=np.matmul(m5G.H(MRCbf),G[:,1:]);
+        dr_MRC=1+pu*nl.norm(mu_int)**2;
+        dr_bound_MRC=dr_bound_MRC+pu*np.sum(beta[1:]);
+        rate_MRC[mx]=rate_MRC[mx]+np.log2(1+nr_MRC/dr_MRC);
+        bound_MRC[mx]=bound_MRC[mx]+np.log2(1+nr_bound_MRC/dr_bound_MRC);
+        
+        GG=np.matmul(m5G.H(G),G);
+        nr_ZF=pu;invGG=nl.inv(GG);
+        dr_ZF=np.real(invGG[0,0]);
+        rate_ZF[mx]=rate_ZF[mx]+np.log2(1+nr_ZF/dr_ZF);    
+
+rate_MRC=rate_MRC/ITER;
+bound_MRC=bound_MRC/ITER;
+rate_ZF=rate_ZF/ITER;
+
+
+rate_MRC = rate_MRC/ITER;
+bound_MRC = bound_MRC/ITER;
+rate_ZF = rate_ZF/ITER;
+
+
+plt.plot(Mv, rate_MRC,'g-');
+plt.plot(Mv, bound_MRC,'rs');
+plt.plot(Mv, rate_ZF,'b^-');
+plt.grid(1,which='both')
+plt.legend(["MRC", "MRC Bound", "ZF"], loc ="lower right");
+plt.suptitle('SINR for MRC and ZF Receivers')
+plt.ylabel('SNR ')
+plt.xlabel('Number of antennas M')
